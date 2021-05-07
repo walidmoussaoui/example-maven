aab49ec87d7ee3ec7fef54370ed450d0339f49d0
MOUSSAOUI Walid <wmoussaoui@generixgroup.com>
2021-05-07 09:56:15 +0200

    initial commit

---
+++ src/main/java/biz/generix/swfexamplemaven/SwfExampleMavenApplication.java
@@ -0,0 +1,13 @@
+package biz.generix.swfexamplemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwfExampleMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwfExampleMavenApplication.class, args);
+	}
+
+}
